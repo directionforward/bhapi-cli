9f95d9935ff971b388f3be0ca27b71409aaf4a03
Direction Forward <info@directionforward.com>
2021-09-10 14:52:53 +0100

    Update bhapi.py

--- bhapi.py
+++ bhapi.py
@@ -26,12 +26,22 @@ class BHApi:
 	bhrest_token = None
 	access_token = None
 	refresh_token = None
+
+	hdr = {
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+		'Accept-Encoding': 'none',
+		'Accept-Language': 'en-US,en;q=0.8',
+		'Connection': 'keep-alive'
+	}
 	
 	def __init__(self):
 		self.logger = BHLogger()
 	
 	def get(self,url,jsonEncode=True):
-		req = urllib2.Request(url)
+		req = urllib2.Request(url,headers=self.hdr)
+		#req = urllib2.Request(url)
 		try:
 			response = urllib2.urlopen(req)
 		except urllib2.HTTPError as e:
@@ -48,7 +58,8 @@ class BHApi:
 		return json_loads(response.read()) if jsonEncode else response
 	
 	def post(self,url,params={},jsonEncode=True):
-		req = urllib2.Request(url)
+		req = urllib2.Request(url,headers=self.hdr)
+		#req = urllib2.Request(url)
 		data = ''
 		if type(params) is not str:
 			data = urllib.urlencode(params)
